246b86234341760642fd34388e79710935d6a607
Yurii PErekrestnyi <jodlei1618@gmail.com>
2023-03-09 00:30:57 +0200

    add auth

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,12 +3,23 @@ const { Contact } = require("../models/contact");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
-  const result = await Contact.find();
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find(
+    { owner },
+    {
+      skip,
+      limit,
+    }
+  ).populate("owner", "name email");
   res.json(result);
 };
 
 const getContactById = async (req, res) => {
-  const result = await Contact.findById(req.params.contactId);
+  const { id } = req.params;
+  const result = await Contact.findById(id);
+
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -16,13 +27,14 @@ const getContactById = async (req, res) => {
 };
 
 const addContactById = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
 const deleteContactById = async (req, res) => {
-  const result = await Contact.findByIdAndRemove(req.params.contactId);
-
+  const { id } = req.params;
+  const result = await Contact.findByIdAndRemove(id);
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -32,25 +44,19 @@ const deleteContactById = async (req, res) => {
 };
 
 const updateContactById = async (req, res) => {
-  const result = await Contact.findByIdAndUpdate(
-    req.params.contactId,
-    req.body,
-    { new: true }
-  );
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
 
   if (!result) {
     throw HttpError(404, "Not found");
   }
+
   res.json(result);
 };
 
 const updateFavorite = async (req, res) => {
-  const result = await Contact.findByIdAndUpdate(
-    req.params.contactId,
-    req.body,
-    { new: true }
-  );
-
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
   if (!result) {
     throw HttpError(404, "Not found");
   }
